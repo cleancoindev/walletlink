16ed60f2d74eb36317d7aeddc82efb86b898541c
Charles Bergeron <chuck.bergeron@gmail.com>
2020-01-15 19:40:50 -0800

    * hard-code the WALLETLINK_URL param to the correct URL

--- web/src/WalletLink/types/SessionConfig.ts
+++
@@ -1,9 +0,0 @@
-// Copyright (c) 2018-2019 WalletLink.org <https://www.walletlink.org/>
-// Copyright (c) 2018-2019 Coinbase, Inc. <https://www.coinbase.com/>
-// Licensed under the Apache License, version 2.0
-
-export interface SessionConfig {
-  webhookId: string
-  webhookUrl: string
-  metadata: { [key: string]: string | undefined }
-}
